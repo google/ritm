bf24e37d5ed5b2173ce8846c6e311fb0cd8b00bb
Mateusz Maćkowski <mmac@google.com>
2025-09-26 15:58:31 +0000

    Add valid copyright headers

--- src/pagetable.rs
+++ src/pagetable.rs
@@ -1,3 +1,11 @@
+// Copyright 2025 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 <LICENSE-APACHE or
+// https://www.apache.org/licenses/LICENSE-2.0> or the MIT license
+// <LICENSE-MIT or https://opensource.org/licenses/MIT>, at your
+// option. This file may not be copied, modified, or distributed
+// except according to those terms.
+
 use crate::platform::PlatformImpl;
 use aarch64_paging::paging::Attributes;
 use aarch64_rt::initial_pagetable;
