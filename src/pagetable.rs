7a78f71a2706a9a4d4884629508aa3ebb27f8755
Mateusz Maćkowski <mmac@google.com>
2026-03-10 17:00:33 +0100

    Add stage-2 pagetables (#16)
    This makes the RITM memory invisible from the guest.

--- src/pagetable.rs
+++ src/pagetable.rs
@@ -7,22 +7,37 @@
 // except according to those terms.
 
 use crate::platform::PlatformImpl;
-use aarch64_paging::descriptor::Attributes;
+use aarch64_paging::descriptor::{El23Attributes, Stage2Attributes};
 use aarch64_rt::initial_pagetable;
 
 /// Attributes to use for device memory in the initial identity map.
-pub const DEVICE_ATTRIBUTES: Attributes = Attributes::VALID
-    .union(Attributes::ATTRIBUTE_INDEX_0)
-    .union(Attributes::ACCESSED)
-    .union(Attributes::UXN);
+pub const DEVICE_ATTRIBUTES: El23Attributes = El23Attributes::VALID
+    .union(El23Attributes::ATTRIBUTE_INDEX_0)
+    .union(El23Attributes::ACCESSED)
+    .union(El23Attributes::XN);
 
 /// Attributes to use for normal memory in the initial identity map.
-pub const MEMORY_ATTRIBUTES: Attributes = Attributes::VALID
-    .union(Attributes::ATTRIBUTE_INDEX_1)
-    .union(Attributes::INNER_SHAREABLE)
-    .union(Attributes::ACCESSED)
-    .union(Attributes::NON_GLOBAL);
+pub const MEMORY_ATTRIBUTES: El23Attributes = El23Attributes::VALID
+    .union(El23Attributes::ATTRIBUTE_INDEX_1)
+    .union(El23Attributes::INNER_SHAREABLE)
+    .union(El23Attributes::ACCESSED)
+    .union(El23Attributes::NON_GLOBAL);
 
 // The initial hardcoded page table used before the Rust code starts and activates the main page
 // table.
 initial_pagetable!(PlatformImpl::initial_idmap());
+
+/// Attributes to use for device memory in the stage-2 page tables.
+pub const STAGE2_DEVICE_ATTRIBUTES: Stage2Attributes = Stage2Attributes::VALID
+    .union(Stage2Attributes::MEMATTR_DEVICE_nGnRnE)
+    .union(Stage2Attributes::S2AP_ACCESS_RW)
+    .union(Stage2Attributes::ACCESS_FLAG)
+    .union(Stage2Attributes::SH_NONE);
+
+/// Attributes to use for normal memory in the stage-2 page tables.
+pub const STAGE2_MEMORY_ATTRIBUTES: Stage2Attributes = Stage2Attributes::VALID
+    .union(Stage2Attributes::MEMATTR_NORMAL_OUTER_WB)
+    .union(Stage2Attributes::MEMATTR_NORMAL_INNER_WB)
+    .union(Stage2Attributes::S2AP_ACCESS_RW)
+    .union(Stage2Attributes::ACCESS_FLAG)
+    .union(Stage2Attributes::SH_INNER);
